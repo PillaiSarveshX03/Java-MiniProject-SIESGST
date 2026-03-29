4d336334f168675fd36be7f8ac1674a3efd407ad
Sarvesh Pillai <pillaisarvesh03@gmail.com>
2026-03-29 14:44:47 +0000

    Learnt File IO and reading characters and lines the analogy i intended worked Automatically lmao : )

--- Tests/File IO/File.java
+++ Tests/File IO/File.java
@@ -1,7 +1,33 @@
 import java.io.*;
+import java.util.Scanner;
+import java.io.BufferedReader;
+
+
 
 public class File {
-    public static void main(String[] args) {
-        // Your code here
+    public static void main(String[] args) throws IOException
+    
+    {
+       BufferedReader reader = new BufferedReader(new FileReader("Movie.txt"));
+        
+       String line;
+       
+        int ch = reader.read();
+        int mov = ch - '0'; 
+
+        System.out.println(mov);
+
+             System.out.println("Now Showing : \n");
+
+    
+        while ((line = reader.readLine()) != null) 
+        {  
+             System.out.println(line);
+        }
+
+       
+
+        reader.close();
     }
 }
+
